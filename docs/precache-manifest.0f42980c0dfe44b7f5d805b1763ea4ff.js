2c014d567843099b3c81a0cd1b1dc96bbe9ada6a
Alexandr Grinevich <devnull@AlexandrsMBP10.fios-router.home>
2019-10-09 18:01:43 -0400

    added docs folder

---
+++ docs/precache-manifest.0f42980c0dfe44b7f5d805b1763ea4ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a9898c25e2f2f3800d1f3be0f8abc93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "534e482fd39d19592ab9",
+    "url": "/static/css/main.f8f988df.chunk.css"
+  },
+  {
+    "revision": "147dbdc59bced535a9b8",
+    "url": "/static/js/2.5bf4ba21.chunk.js"
+  },
+  {
+    "revision": "534e482fd39d19592ab9",
+    "url": "/static/js/main.0ee666dd.chunk.js"
+  },
+  {
+    "revision": "2e47bfd859accdc53367",
+    "url": "/static/js/runtime-main.080cdf64.js"
+  },
+  {
+    "revision": "cb61ac4fca1dd4fbbcb5a2e7b3403d5c",
+    "url": "/static/media/noResults.cb61ac4f.svg"
+  }
+]);
