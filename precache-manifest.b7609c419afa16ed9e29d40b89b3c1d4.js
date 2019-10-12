513f0022338bc71e64c47b63e19a501f06ae4aa5
Alexandr Grinevich <devnull@AlexandrsMBP10.fios-router.home>
2019-10-11 21:17:23 -0400

    Updates

---
+++ precache-manifest.b7609c419afa16ed9e29d40b89b3c1d4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a606092ac958b10fa965ae60ffea653",
+    "url": "/index.html"
+  },
+  {
+    "revision": "982559a17f57b8dad800",
+    "url": "/static/css/main.70bc9a4f.chunk.css"
+  },
+  {
+    "revision": "db94665e3981a063f457",
+    "url": "/static/js/2.59d8d02b.chunk.js"
+  },
+  {
+    "revision": "982559a17f57b8dad800",
+    "url": "/static/js/main.cb8e93f0.chunk.js"
+  },
+  {
+    "revision": "4085e0dbb660425dd33c",
+    "url": "/static/js/runtime-main.809b04c2.js"
+  },
+  {
+    "revision": "fd5ffbbf6ae3076bd2a049e76cd25740",
+    "url": "/static/media/cup.fd5ffbbf.svg"
+  },
+  {
+    "revision": "7766baa51065710da08a5d2d5bd48b31",
+    "url": "/static/media/material-ui.7766baa5.svg"
+  },
+  {
+    "revision": "cb61ac4fca1dd4fbbcb5a2e7b3403d5c",
+    "url": "/static/media/noResults.cb61ac4f.svg"
+  },
+  {
+    "revision": "6c3d421cc84ae37f8a4cb510d6ce10bd",
+    "url": "/static/media/nodejs-icon.6c3d421c.svg"
+  },
+  {
+    "revision": "c6e924c06d7f5ca1d139fae16a6bae36",
+    "url": "/static/media/react-1.c6e924c0.svg"
+  }
+]);
